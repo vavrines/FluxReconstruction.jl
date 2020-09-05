7020a1ea7c4ffb2a3838e26d0fa8c5ef99d2568c
vavrines <tianbaixiao@gmail.com>
2020-09-05 12:33:21 +0200

    Revise & Add example

--- src/geo.jl
+++ src/geo.jl
@@ -3,8 +3,8 @@ Calculate global location of solution points
 
 """
 function global_sp(xi::AbstractArray{<:Real,1}, r::AbstractArray{<:Real,1})
-    xsp = similzr(xi, length(xi) - 1, length(r))
-    for i = 1:length(xi)-1, j in axes(r)
+    xsp = similar(xi, length(xi) - 1, length(r))
+    for i in 1:length(xi)-1, j in axes(r, 1)
         xsp[i, j] = ((1.0 - r[j]) / 2.0) * xi[i] + ((1.0 + r[j]) / 2.0) * xi[i+1]
     end
 
