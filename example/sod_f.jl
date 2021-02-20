ada69f1fbceae08f96a44fb45e3b5d73583cd75e
vavrines <tianbaixiao@gmail.com>
2021-02-20 21:28:50 +0100

    Sync

--- example/sod_f.jl
+++ example/sod_f.jl
@@ -35,140 +35,145 @@ begin
 end
 
 w = zeros(nx, 3, nsp)
-f = zeros(nx, nu, nsp)
+h = zeros(nx, nu, nsp)
+b = zeros(nx, nu, nsp)
 for i = 1:nx, ppp1 = 1:nsp
     if i <= nx÷2
         _ρ = 1.0
         _λ = 0.5
     else
-        _ρ = 0.3#0.125
+        _ρ = 0.125
         _λ = 0.625
     end
 
-    w[i, :, ppp1] .= prim_conserve([_ρ, 0.0, _λ], 3.0)
-    f[i, :, ppp1] .= maxwellian(vspace.u, [_ρ, 1.0, _λ])
-end
-
-e2f = zeros(Int, nx, 2)
-for i = 1:nx
-    if i == 1
-        e2f[i, 2] = nface
-        e2f[i, 1] = i + 1
-    elseif i == nx
-        e2f[i, 2] = i
-        e2f[i, 1] = 1
-    else
-        e2f[i, 2] = i
-        e2f[i, 1] = i + 1
-    end
-end
-f2e = zeros(Int, nface, 2)
-for i = 1:nface
-    if i == 1
-        f2e[i, 1] = i
-        f2e[i, 2] = nx
-    elseif i == nface
-        f2e[i, 1] = 1
-        f2e[i, 2] = i - 1
-    else
-        f2e[i, 1] = i
-        f2e[i, 2] = i - 1
-    end
+    w[i, :, ppp1] .= prim_conserve([_ρ, 0.0, _λ], 5/3)
+    h[i, :, ppp1] .= maxwellian(vspace.u, [_ρ, 1.0, _λ])
+    @. b[i, :, ppp1] = h[i, :, ppp1] * 2.0 / 2.0 / _λ
 end
 
 function mol!(du, u, p, t) # method of lines
-    dx, e2f, f2e, velo, weights, δ, deg, ll, lr, lpdm, dgl, dgr = p
+    dx, velo, weights, δ, ll, lr, lpdm, dgl, dgr = p
 
-    w = @view u[:, 1:3, :]
-    pdf = @view u[:, 4:end, :]
+    ncell = length(dx)
+    nu = length(velo)
+    nsp = length(ll)
 
-    ncell = size(pdf, 1)
-    nu = size(pdf, 2)
-    nsp = size(pdf, 3)
+    h = @view u[:, 1:nu, :]
+    b = @view u[:, nu+1:end, :]
 
-    τ = 0.01
+    M = similar(u, ncell, 2*nu, nsp)
+    @inbounds Threads.@threads for k = 1:nsp
+        for i = 1:ncell
+            w = [
+                sum(@. weights * u[i, 1:nu, k]),
+                sum(@. weights * velo * u[i, 1:nu, k]),
+                0.5 * (sum(@. weights * velo^2 * u[i, 1:nu, k]) + sum(@. weights * u[i, nu+1:end, k]))
+            ]
 
-    f = similar(u)
+            prim = conserve_prim(w, 5/3)
+            M[i, 1:nu, k] .= maxwellian(velo, prim)
+            M[i, nu+1:end, k] .= M[i, 1:nu, k] / prim[end]
+        end
+    end
+
+    τ = 0.001#001
+
+    f = zero(u)
     @inbounds Threads.@threads for i = 1:ncell
         J = 0.5 * dx[i]
 
         for k = 1:nsp
-            @. f[i, 4:end, k] = velo * pdf[i, :, k] / J
-
-            f[i, 1, k] = sum(weights .* f[i, 4:end, k])
-            f[i, 2, k] = sum(weights .* velo .* f[i, 4:end, k])
-            f[i, 3, k] = 0.5 * sum(weights .* velo .^ 2 .* f[i, 4:end, k])
+            @. f[i, 1:nu, k] = velo * h[i, :, k] / J
+            @. f[i, nu+1:end, k] = velo * b[i, :, k] / J
         end
     end
 
-    f_face = zeros(eltype(u), ncell, nu+3, 2)
-    @inbounds Threads.@threads for i = 1:ncell 
-        for j = 1:nu+3, k = 1:nsp
+    u_face = zeros(eltype(u), ncell, 2*nu, 2)
+    f_face = zeros(eltype(u), ncell, 2*nu, 2)
+    @inbounds Threads.@threads for i = 1:ncell
+        for j = 1:2*nu, k = 1:nsp
             # right face of element i
+            u_face[i, j, 1] += u[i, j, k] * lr[k]
             f_face[i, j, 1] += f[i, j, k] * lr[k]
 
             # left face of element i
+            u_face[i, j, 2] += u[i, j, k] * ll[k]
             f_face[i, j, 2] += f[i, j, k] * ll[k]
         end
     end
 
-    f_interaction = similar(u, nface, nu+3)
+    u_interaction = zeros(eltype(u), nface, 2*nu)
+    f_interaction = zeros(eltype(u), nface, 2*nu)
     @inbounds Threads.@threads for i = 2:nface-1
-        @. f_interaction[i, 4:end] =
-            f_face[f2e[i, 1], 4:end, 2] * (1.0 - δ) + f_face[f2e[i, 2], 4:end, 1] * δ
-
-        f_interaction[i, 1] = sum(weights .* f_interaction[i, 4:end])
-        f_interaction[i, 2] = sum(weights .* velo .* f_interaction[i, 4:end])
-        f_interaction[i, 3] = 0.5 * sum(weights .* velo .^ 2 .* f_interaction[i, 4:end])
+        @. u_interaction[i, 1:nu] = u_face[i, 1:nu, 2] * (1.0 - δ) + u_face[i-1, 1:nu, 1] * δ
+        @. u_interaction[i, nu+1:end] = u_face[i, nu+1:end, 2] * (1.0 - δ) + u_face[i-1, nu+1:end, 1] * δ
+        @. f_interaction[i, 1:nu] = f_face[i, 1:nu, 2] * (1.0 - δ) + f_face[i-1, 1:nu, 1] * δ
+        @. f_interaction[i, nu+1:end] = f_face[i, nu+1:end, 2] * (1.0 - δ) + f_face[i-1, nu+1:end, 1] * δ
     end
 
-    rhs1 = zeros(eltype(u), ncell, nu+3, nsp)
-    @inbounds Threads.@threads for i = 2:ncell-1
-        for j = 1:nu+3, ppp1 = 1:nsp, k = 1:nsp
-            rhs1[i, j, ppp1] += f[i, j, k] * lpdm[ppp1, k]
+    rhs = zeros(eltype(u), ncell, 2*nu, nsp)
+    @inbounds Threads.@threads for i = 1:ncell
+        for j = 1:2*nu, ppp1 = 1:nsp, k = 1:nsp
+            rhs[i, j, ppp1] += f[i, j, k] * lpdm[ppp1, k]
+        end
+    end
+
+    rhs1 = zeros(eltype(u), ncell, 2*nu, nsp)
+    @inbounds Threads.@threads for i = 1:ncell
+        for j = 1:2*nu, ppp1 = 1:nsp, k = 1:nsp
+            rhs1[i, j, ppp1] += u[i, j, k] * lpdm[ppp1, k]
+        end
+        #=
+        for j = 1:n2, ppp1 = 1:nsp
+            rhs1[i, j, ppp1] += 
+                (u_interaction[i, j] - u_face[i, j, 2]) * dgl[ppp1] +
+                (u_interaction[i+1, j] - u_face[i, j, 1]) * dgr[ppp1]
+        end=#
+    end
+    rhs2 = zeros(eltype(u), ncell, 2*nu, nsp)
+    @inbounds Threads.@threads for i = 1:ncell
+        for j = 1:2*nu, ppp1 = 1:nsp, k = 1:nsp
+            rhs2[i, j, ppp1] += rhs1[i, j, k] * lpdm[ppp1, k]
         end
     end
 
     @inbounds Threads.@threads for i = 2:ncell-1
-        for ppp1 = 1:nsp
-            j = 1:3
-            @. du[i, j, ppp1] =
+        for ppp1 = 1:nsp, j = 1:2*nu
+            du[i, j, ppp1] =
                 -(
-                    rhs1[i, j, ppp1] +
-                    (f_interaction[e2f[i, 2], j] - f_face[i, j, 2]) * dgl[ppp1] +
-                    (f_interaction[e2f[i, 1], j] - f_face[i, j, 1]) * dgr[ppp1]
-                )
+                    rhs[i, j, ppp1] +
+                    (f_interaction[i, j] - f_face[i, j, 2]) * dgl[ppp1] +
+                    (f_interaction[i+1, j] - f_face[i, j, 1]) * dgr[ppp1]
+                ) + (M[i, j, ppp1] - u[i, j, ppp1]) / τ
 
-            j = 4:nu+3
-            du[i, j, ppp1] .=
-                -(
-                    rhs1[i, j, ppp1] .+
-                    (f_interaction[e2f[i, 2], j] .- f_face[i, j, 2]) .* dgl[ppp1] .+
-                    (f_interaction[e2f[i, 1], j] .- f_face[i, j, 1]) .* dgr[ppp1]
-                ) .+ 
-                (maxwellian(velo, conserve_prim(u[i, 1:3, ppp1], 3.0)) - u[i, j, ppp1]) / τ
+            # artifical viscosity
+            if true#abs(rhs1[i, 1, ppp1] / u[i, 1, ppp1]) > dx[i] * 1
+                @. du[i, :, ppp1] += 3e1 * rhs2[i, :, ppp1]
+            end
         end
     end
-
+    du[1, :, :] .= 0.0
+    du[ncell, :, :] .= 0.0
 end
 
-u0 = zeros(nx, nu+3, nsp)
+u0 = zeros(nx, 2*nu, nsp)
 for i in axes(u0, 1), k in axes(u0, 3)
-    for j in 1:3
-        u0[i, j, k] = w[i, j, k]
-    end
-    for j in 4:nu+3
-        u0[i, j, k] = f[i, j-3, k]
-    end
+    j = 1:nu
+    u0[i, j, k] .= h[i, :, k]
+
+    j = nu+1:2*nu
+    u0[i, j, k] .= b[i, :, k]
 end
 
-tspan = (0.0, 0.01)
-p = (pspace.dx, e2f, f2e, vspace.u, vspace.weights, δ, deg, ll, lr, lpdm, dgl, dgr)
+tspan = (0.0, 0.12)
+nt = floor(tspan[2] / dt) |> Int
+p = (pspace.dx, vspace.u, vspace.weights, δ, ll, lr, lpdm, dgl, dgr)
 
 prob = ODEProblem(mol!, u0, tspan, p)
-sol = solve(
+# integrator
+itg = init(
     prob,
-    Midpoint(),
+    Euler(),
     #TRBDF2(),
     #KenCarp3(),
     #KenCarp4(),
@@ -176,23 +181,45 @@ sol = solve(
     #reltol = 1e-8,
     #abstol = 1e-8,
     adaptive = false,
-    dt = dt/100,
+    dt = dt,
     progress = true,
     progress_steps = 10,
     progress_name = "frode",
     #autodiff = false,
 )
 
-prim0 = zeros(nx, 3, nsp)
-prim = zeros(nx, 3, nsp)
-for i = 1:nx, j = 1:nsp
-    _w0 = w[i, :, j]
-    _w = sol.u[end][i, 1:3, j]
-
-    prim0[i, :, j] .= conserve_prim(_w0, 3)
-    prim[i, :, j] .= conserve_prim(_w, 3)
+@showprogress for iter = 1:nt
+    step!(itg)
+    itg.u[1,:,:] .= itg.u[2,:,:]
+    itg.u[nx,:,:] .= itg.u[nx-1,:,:]
 end
 
-plot(xsp[:, 2], prim0[:, 1, 2])
-plot!(xsp[:, 2], prim[:, 1, 2])
-plot!(xsp[:, 2], 1 ./ prim[:, 3, 2])
+u = deepcopy(u0)
+du = zero(u)
+@showprogress for iter = 1:nt
+    mol!(du, u, p, t)
+    u .+= du * dt
+    u[1,:,:] .= u[2,:,:]
+    u[nx,:,:] .= u[nx-1,:,:]
+end
+
+begin
+    x = zeros(nx * nsp)
+    prim = zeros(nx * nsp, 3)
+    for i = 1:nx
+        idx0 = (i - 1) * nsp
+        idx = idx0+1:idx0+nsp
+
+        for j = 1:nsp
+            idx = idx0 + j
+            x[idx] = xsp[i, j]
+
+            _h = itg.u[i, 1:nu, j]
+            _b = itg.u[i, nu+1:end, j]
+            _w = moments_conserve(_h, _b, vspace.u, vspace.weights)
+            prim[idx, :] .= conserve_prim(_w, 5/3)
+        end
+    end
+    plot(x, prim[:, 1])
+    plot!(x, 1 ./ prim[:, 3])
+end
