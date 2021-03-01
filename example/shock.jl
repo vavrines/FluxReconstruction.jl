765c77b2e210f39f60ef5bcd44702a4d61590a21
vavrines <tianbaixiao@gmail.com>
2021-03-01 10:02:58 +0100

    Update shock.jl

--- example/shock.jl
+++ example/shock.jl
@@ -1,8 +1,8 @@
-using KitBase, OrdinaryDiffEq, Plots, LinearAlgebra
-import FluxRC
+using KitBase, OrdinaryDiffEq, Plots, ProgressMeter, JLD2
 using Logging: global_logger
 using TerminalLoggers: TerminalLogger
 global_logger(TerminalLogger())
+import FluxRC
 
 begin
     x0 = -25
@@ -12,16 +12,16 @@ begin
     dx = (x1 - x0) / nx
     deg = 2 # polynomial degree
     nsp = deg + 1
-    u0 = -8
-    u1 = 8
-    nu = 24
-    v0 = -8
-    v1 = 8
-    nv = 24
-    w0 = -8
-    w1 = 8
-    nw = 24
-    cfl = 0.2
+    u0 = -10
+    u1 = 10
+    nu = 48
+    v0 = -10
+    v1 = 10
+    nv = 28
+    w0 = -10
+    w1 = 10
+    nw = 28
+    cfl = 0.15
     dt = cfl * dx / u1
     t = 0.0
     mach = 2.0
@@ -58,7 +58,6 @@ phi, psi, phipsi = kernel_mode(
     1.0,
 )
 
-
 ib = ib_rh(mach, 5/3, vspace.u, vspace.v, vspace.w)
 
 u0 = zeros(nx, nu, nv, nw, nsp)
@@ -73,14 +72,14 @@ for i = 1:nx, ppp1 = 1:nsp
 end
 
 function mol!(du, u, p, t)
-    dx, vx, vy, vz, weights, δ, ll, lr, lpdm, dgl, dgr = p
+    dx, vx, vy, vz, weights, δ, Kn, nm, phi, psi, phipsi, ll, lr, lpdm, dgl, dgr = p
 
     ncell = length(dx)
     nu = size(vx, 1)
     nv = size(vy, 2)
     nw = size(vz, 3)
     nsp = length(ll)
-
+#=
     M = similar(u, ncell, nu, nv, nw, nsp)
     @inbounds Threads.@threads for k = 1:nsp
         for i = 1:ncell
@@ -97,6 +96,20 @@ function mol!(du, u, p, t)
         end
     end
     τ = 1.0
+=#
+    Q = zero(u)
+    @inbounds Threads.@threads for k = 1:nsp
+        for i = 1:ncell
+            Q[i, :, :, :, k] .= boltzmann_fft(
+                u[i, :, :, :, k],
+                Kn,
+                nm,
+                phi,
+                psi,
+                phipsi,
+            )
+        end
+    end
 
     f = similar(u)
     @inbounds Threads.@threads for k = 1:nsp
@@ -137,7 +150,7 @@ function mol!(du, u, p, t)
                     rhs1[i, :, :, :, ppp1] .+
                     (f_interaction[i, :, :, :] .- f_face[i, :, :, :, 2]) .* dgl[ppp1] .+
                     (f_interaction[i+1, :, :, :] - f_face[i, :, :, :, 1]) .* dgr[ppp1]
-                ) .+ (M[i, :, :, :, ppp1] .- u[i, :, :, :, ppp1]) ./ τ
+                ) .+ Q[i, :, :, :, ppp1]#(M[i, :, :, :, ppp1] .- u[i, :, :, :, ppp1]) ./ τ
         end
     end
     du[1, :, :, :, :] .= 0.0
@@ -145,17 +158,20 @@ function mol!(du, u, p, t)
 
 end
 
-tspan = (0.0, 5.0)
-p = (pspace.dx, vspace.u, vspace.v, vspace.w, vspace.weights, δ, ll, lr, lpdm, dgl, dgr)
+tspan = (0.0, 100.0)
+p = (pspace.dx, vspace.u, vspace.v, vspace.w, vspace.weights, δ, 
+    kn_bzm, 5, phi, psi, phipsi, ll, lr, lpdm, dgl, dgr)
 
 prob = ODEProblem(mol!, u0, tspan, p)
+
 sol = solve(
     prob,
-    Midpoint(),
+    Euler(),
     #TRBDF2(),
     #KenCarp3(),
     #KenCarp4(),
     saveat = tspan[2],
+    #save_everystep = false,
     #reltol = 1e-8,
     #abstol = 1e-8,
     adaptive = false,
@@ -165,8 +181,10 @@ sol = solve(
     progress_name = "frode",
     #autodiff = false,
 )
-prob = remake(prob, u0=sol.u[end], tspan=tspan, p=p)
+#prob = remake(prob, u0=sol.u[end], tspan=tspan, p=p)
 
+@save "sol_shock.jld2" sol
+#=
 begin
     prim = zeros(nx, 5, nsp)
     for i = 1:nx, j = 1:nsp
@@ -175,4 +193,4 @@ begin
     end
     plot(xsp[:, 2], prim[:, 1:2, 2])
     plot!(xsp[:, 2], 1 ./ prim[:, end, 2])
-end
+end=#
