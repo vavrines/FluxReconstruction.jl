abf73d8010c9a891d062207fdb02f53d4a53692e
vavrines <tianbaixiao@gmail.com>
2021-03-15 11:44:47 +0100

    Finish cavity case

--- dev/cavity/vis.jl
+++ dev/cavity/vis.jl
@@ -1,7 +1,8 @@
-using BSON, DataFrames, KitBase, KitBase.Plots, KitBase.CSV, PyPlot
+using BSON, DataFrames, KitBase, KitBase.CSV, KitBase.PyCall, KitBase.Plots, PyPlot
 import FluxRC
 
 cd(@__DIR__)
+itp = pyimport("scipy.interpolate")
 BSON.@load "sol.bson" u
 vline = CSV.File("dsmc_vline.csv") |> DataFrame
 hline = CSV.File("dsmc_hline.csv") |> DataFrame
@@ -51,53 +52,57 @@ begin
     end
 end
 
+begin
+    x_uni = coord[1, 1, 1]:(coord[end, 1, 1] - coord[1, 1, 1]) / (nx * nsp - 1):coord[end, 1, 1] |> collect
+    y_uni = coord[1, 1, 2]:(coord[1, end, 2] - coord[1, 1, 2]) / (ny * nsp - 1):coord[1, end, 2] |> collect
+
+    u_ref = itp.interp2d(coord[:, 1, 1], coord[1, :, 2], prim[:, :, 2], kind="cubic")
+    u_uni = u_ref(x_uni, y_uni)
+
+    v_ref = itp.interp2d(coord[:, 1, 1], coord[1, :, 2], prim[:, :, 3], kind="cubic")
+    v_uni = v_ref(x_uni, y_uni)
+
+    t_ref = itp.interp2d(coord[:, 1, 1], coord[1, :, 2], prim[:, :, 4], kind="cubic")
+    t_uni = t_ref(x_uni, y_uni)
+
+    qx_ref = itp.interp2d(coord[:, 1, 1], coord[1, :, 2], prim[:, :, 5], kind="cubic")
+    qx_uni = qx_ref(x_uni, y_uni)
+
+    qy_ref = itp.interp2d(coord[:, 1, 1], coord[1, :, 2], prim[:, :, 6], kind="cubic")
+    qy_uni = qy_ref(x_uni, y_uni)
+end
+
 fig = figure("contour", figsize=(6.5,5))
-PyPlot.contourf(coord[1:end, 1, 1], coord[1, 1:end, 2], prim[:, :, 2]', linewidth=1, levels=20, cmap=ColorMap("inferno"))
+PyPlot.contourf(x_uni, y_uni, u_uni', linewidth=1, levels=20, cmap=ColorMap("inferno"))
 colorbar()
-PyPlot.streamplot(coord[1:end, 1, 1], coord[1, 1:end, 2], prim[:, :, 2]', prim[:, :, 3]', density=1.3, color="moccasin", linewidth=1)
-xlabel("X")
-ylabel("Y")
+PyPlot.streamplot(x_uni, y_uni, u_uni', v_uni', density=1.3, color="moccasin", linewidth=1)
+xlabel("x")
+ylabel("y")
 #PyPlot.title("U-velocity")
 xlim(0.01,0.99)
 ylim(0.01,0.99)
 #PyPlot.grid("on")
-
-
-
-
-
-
-
-
-
 display(fig)
+fig.savefig("cavity_u.pdf")
 
-fig.savefig("cavity_EU_kn1.pdf")
+close("all")
+fig = figure("contour", figsize=(6.5,5))
+PyPlot.contourf(x_uni, y_uni, t_uni', linewidth=1, levels=20, cmap=ColorMap("inferno"))
+colorbar()
+PyPlot.streamplot(x_uni, y_uni, qx_uni', qy_uni', density=1.3, color="moccasin", linewidth=1)
+xlabel("x")
+ylabel("y")
+#PyPlot.title("U-velocity")
+xlim(0.01,0.99)
+ylim(0.01,0.99)
+#PyPlot.grid("on")
+display(fig)
+fig.savefig("cavity_t.pdf")
 
+Plots.plot(prim[23, :, 2] ./ 0.15, coord[23, 1:end, 2], lw=2, label="current", xlabel="U/Uw", ylabel="y")
+Plots.scatter!(vline.u, vline.y, label="DSMC")
+Plots.savefig("cavity_vline.pdf")
 
-
-
-
-
-
-
-
-
-
-contourf(coord[1:end, 1, 1], coord[1, 1:end, 2], _prim[:, :, 2]')
-
-
-
-
-
-
-
-
-
-
-
-plot(_prim[23, :, 2] ./ 0.15, coord[23, 1:end, 2], label="current")
-scatter!(vline.u, vline.y, label="DSMC")
-
-plot(coord[1:end, 23, 1], _prim[:, 23, 3] ./ 0.15, label="current")
-scatter!(hline.x, hline.v, label="DSMC")
+Plots.plot(coord[1:end, 23, 1], prim[:, 23, 3] ./ 0.15, lw=2, label="current", xlabel = "x", ylabel="V/Uw")
+Plots.scatter!(hline.x, hline.v, label="DSMC")
+Plots.savefig("cavity_hline.pdf")
