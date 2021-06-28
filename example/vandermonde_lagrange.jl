f39f9d760464125ef620d585f92a8327948c5b97
vavrines <tianbaixiao@gmail.com>
2021-06-28 15:30:57 +0200

    Add vandermonde example in 1D

--- example/vandermonde_lagrange.jl
+++ example/vandermonde_lagrange.jl
@@ -11,3 +11,13 @@ for i = 1:ps.deg+1
 end
 
 @test ∂l ≈ ps.dl
+
+ψf = vandermonde_matrix(ps.deg, [-1.0, 1.0])
+
+lf = zeros(2, ps.deg+1)
+for i in axes(lf, 1)
+    lf[i, :] .= V' \ ψf[i, :]
+end
+
+@test lf[1, :] ≈ ps.ll
+@test lf[2, :] ≈ ps.lr
