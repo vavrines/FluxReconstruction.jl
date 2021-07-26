1156b2e587560fea7f08dca9299f3d2b56b65094
vavrines <tianbaixiao@gmail.com>
2021-07-26 22:28:45 +0200

    Extend test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,11 +4,13 @@ cd(@__DIR__)
 
 let u = rand(3), u0 = rand(3) 
     FR.L1_error(u, u0, 0.1)
-    FR.L1_error(u, u0, 0.1)
-    FR.L1_error(u, u0, 0.1)
+    FR.L2_error(u, u0, 0.1)
+    FR.L∞_error(u, u0, 0.1)
 end
 
+shock_detector(-Inf, 3)
 shock_detector(log10(0.1), 3)
+shock_detector(log10(1e5), 3)
 
 deg = 5
 
@@ -29,6 +31,13 @@ let u = rand(deg+1)
     FR.filter_exp(2, 10, Array(ps.V))
 end
 
+pl, wl = tri_quadrature(3)
+V = vandermonde_matrix(3, pl[:, 1], pl[:, 2])
+Vr, Vs = ∂vandermonde_matrix(3, pl[:, 1], pl[:, 2])
+∂l = ∂lagrange(V, Vr, Vs)
+
 f = randn(5, deg+1)
 fδ = randn(5, 2)
 FR.interp_face!(fδ, f, ps.ll, ps.lr)
+
+FR.standard_lagrange(ps.xpl)
