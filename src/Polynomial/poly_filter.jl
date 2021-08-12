4bc7bc06bffc7c36667eb0da7062c89a1706de6d
vavrines <tianbaixiao@gmail.com>
2021-08-12 20:46:41 +0200

    Add 2d filters

--- src/Polynomial/poly_filter.jl
+++ src/Polynomial/poly_filter.jl
@@ -23,6 +23,20 @@ function filter_l2!(u::AbstractVector{T}, args...) where {T<:AbstractFloat}
     return nothing
 end
 
+function filter_l2!(u::AbstractMatrix{T}, args...) where {T<:AbstractFloat}
+    p0 = axes(u, 1) |> first
+    q0 = axes(u, 2) |> first
+    @assert p0 >= 0
+    @assert q0 >= 0
+
+    λx, λξ = args[1:2]
+    for j in axes(u, 2), i in axes(u, 1)
+        u[i, j] /= (1.0 + λx * (i - p0 + 1)^2 * (i - p0)^2 + λξ * (j - q0 + 1)^2 * (j - q0)^2)
+    end
+
+    return nothing
+end
+
 function filter_l2opt!(u::AbstractVector{T}, args...) where {T<:AbstractFloat}
     q0 = eachindex(u) |> first
     q1 = eachindex(u) |> last
@@ -37,6 +51,33 @@ function filter_l2opt!(u::AbstractVector{T}, args...) where {T<:AbstractFloat}
     return nothing
 end
 
+function filter_l2opt!(u::AbstractMatrix{T}, args...) where {T<:AbstractFloat}
+    p0 = axes(u, 1) |> first
+    q0 = axes(u, 2) |> first
+    @assert p0 >= 0
+    @assert q0 >= 0
+
+    λx, λξ = args[1:2]
+    η0 = λx * 2.0^2 + λξ * 2.0^2
+    for j in axes(u, 2)
+        for i in axes(u, 1)
+            if i == p0 && j == q0
+                continue
+            elseif i == 1
+                η = λξ * 2.0^2
+            elseif j == 1
+                η = λx * 2.0^2
+            else
+                η = η0
+            end
+
+            u[i, j] /= (1.0 + λx * (i - p0 + 1)^2 * (i - p0)^2 + λξ * (j - q0 + 1)^2 * (j - q0)^2 - η)
+        end
+    end
+
+    return nothing
+end
+
 function filter_l1!(u::AbstractVector{T}, args...) where {T<:AbstractFloat}
     q0 = eachindex(u) |> first
     q1 = eachindex(u) |> last
@@ -45,7 +86,7 @@ function filter_l1!(u::AbstractVector{T}, args...) where {T<:AbstractFloat}
     λ = args[1]
     ℓ = args[2]
     for i = q0+1:q1
-        sc = 1.0 - λ * i * (i - 1) * ℓ[i] / abs(u[i] + 1e-8)
+        sc = 1.0 - λ * i * (i - 1) * ℓ[i] / (abs(u[i]) + 1e-8)
         if sc < 0.0
             sc = 0.0
         end
@@ -55,15 +96,44 @@ function filter_l1!(u::AbstractVector{T}, args...) where {T<:AbstractFloat}
     return nothing
 end
 
+function filter_l1!(u::AbstractMatrix{T}, args...) where {T<:AbstractFloat}
+    p0 = axes(u, 1) |> first
+    q0 = axes(u, 2) |> first
+    @assert p0 >= 0
+    @assert q0 >= 0
+
+    λ1, λ2 = args[1:2]
+    ℓ = args[3]
+    for j in axes(u, 2), i in axes(u, 1)
+        sc = 1.0 - (λ1 * i * (i - 1) + λ2 * j * (j - 1)) * ℓ[i] / (abs(u[i,j]) + 1e-8)
+        if sc < 0.0
+            sc = 0.0
+        end
+        u[i, j] *= sc
+    end
+
+    return nothing
+end
+
 function filter_lasso!(u::AbstractVector{T}, args...) where {T<:AbstractFloat}
     q0 = eachindex(u) |> first
-    q1 = eachindex(u) |> last
     @assert q0 >= 0
 
     ℓ = args[1]
     nr = length(u)
     λ = abs(u[end]) / (nr * (nr - 1) * ℓ[end])
     filter_l1!(u, λ, ℓ)
+
+    return nothing
+end
+
+function filter_lasso!(u::AbstractMatrix{T}, args...) where {T<:AbstractFloat}
+    nr, nz = size(u)
+    ℓ = args[1]
+    λ1 = abs(u[end, 1]) / (nr * (nr - 1) * ℓ[end, 1])
+    λ2 = abs(u[1, end]) / (nz * (nz - 1) * ℓ[1, end])
+
+    filter_l1!(u, λ1, λ2, ℓ)
 
     return nothing
 end
