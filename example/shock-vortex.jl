dd6202edc45c404d4c7a5270d9ca93df7f534dde
vavrines <tianbaixiao@gmail.com>
2021-09-07 11:15:33 +0200

    Remove unused

--- example/shock-vortex.jl
+++ example/shock-vortex.jl
@@ -2,16 +2,12 @@ using KitBase, FluxReconstruction, LinearAlgebra, OrdinaryDiffEq, OffsetArrays
 using ProgressMeter: @showprogress
 using Base.Threads: @threads
 using Plots
-using NodesAndModes
-
-pyplot()
-cd(@__DIR__)
 
 begin
     set = Setup(
         "gas",
         "cylinder",
-        "2d0f",
+        "2d0f0v",
         "hll",
         "nothing",
         1, # species
@@ -21,13 +17,11 @@ begin
         0.1, # cfl
         1.0, # time
     )
-
-    ps = FRPSpace2D(0.0, 2.0, 100, 0.0, 1.0, 50, set.interpOrder - 1, 1, 1)
-
+    ps = FRPSpace2D(0.0, 2.0, 100, 0.0, 1.0, 50, set.interpOrder-1, 1, 1)
     vs = nothing
     gas = Gas(
         1e-6,
-        1.1, # Mach
+        1.12, # Mach
         1.0,
         3.0, # K
         7 / 5,
@@ -39,12 +33,6 @@ begin
 
     ks = SolverSet(set, ps, vs, gas, ib)
 end
-
-r = hcat(ps.xpl, ps.xpl, ps.xpl)
-s = permutedims(r)
-
-V = basis(Quad(), 2, r[:], s[:])[1]
-iV = inv(V)
 
 function dudt!(du, u, p, t)
     du .= 0.0
@@ -151,21 +139,23 @@ function dudt!(du, u, p, t)
     return nothing
 end
 
-f = OffsetArray{Float64}(
-    undef,
-    0:ks.ps.nx+1,
-    0:ks.ps.ny+1,
-    ks.ps.deg + 1,
-    ks.ps.deg + 1,
-    4,
-    2,
-)
-u_face = OffsetArray{Float64}(undef, 0:ks.ps.nx+1, 0:ks.ps.ny+1, 4, ks.ps.deg + 1, 4)
-f_face = OffsetArray{Float64}(undef, 0:ks.ps.nx+1, 0:ks.ps.ny+1, 4, ks.ps.deg + 1, 4, 2)
-fx_interaction = zeros(ks.ps.nx + 1, ks.ps.ny, ks.ps.deg + 1, 4)
-fy_interaction = zeros(ks.ps.nx, ks.ps.ny + 1, ks.ps.deg + 1, 4)
-rhs1 = zeros(ks.ps.nx, ks.ps.ny, ks.ps.deg + 1, ks.ps.deg + 1, 4)
-rhs2 = zeros(ks.ps.nx, ks.ps.ny, ks.ps.deg + 1, ks.ps.deg + 1, 4)
+begin
+    f = OffsetArray{Float64}(
+        undef,
+        0:ks.ps.nx+1,
+        0:ks.ps.ny+1,
+        ks.ps.deg + 1,
+        ks.ps.deg + 1,
+        4,
+        2,
+    )
+    u_face = OffsetArray{Float64}(undef, 0:ks.ps.nx+1, 0:ks.ps.ny+1, 4, ks.ps.deg + 1, 4)
+    f_face = OffsetArray{Float64}(undef, 0:ks.ps.nx+1, 0:ks.ps.ny+1, 4, ks.ps.deg + 1, 4, 2)
+    fx_interaction = zeros(ks.ps.nx + 1, ks.ps.ny, ks.ps.deg + 1, 4)
+    fy_interaction = zeros(ks.ps.nx, ks.ps.ny + 1, ks.ps.deg + 1, 4)
+    rhs1 = zeros(ks.ps.nx, ks.ps.ny, ks.ps.deg + 1, ks.ps.deg + 1, 4)
+    rhs2 = zeros(ks.ps.nx, ks.ps.ny, ks.ps.deg + 1, ks.ps.deg + 1, 4)
+end
 
 p = (
     f,
@@ -191,109 +181,175 @@ nt = tspan[2] ÷ dt |> Int
 # initial condition
 u0 = OffsetArray{Float64}(undef, 0:ps.nx+1, 0:ps.ny+1, ps.deg + 1, ps.deg + 1, 4)
 for i in axes(u0, 1), j in axes(u0, 2), k in axes(u0, 3), l in axes(u0, 4)
-    ρ = ks.gas.Ma^2
+    t1, t2 = ib_rh(ks.gas.Ma, ks.gas.γ)
 
-    if ps.x[i, j] <= ps.x1 / 4
-        t1 = ib_rh(ks.gas.Ma, ks.gas.γ, rand(3))[2]
-        prim = [t1[1], t1[2], 0.0, t1[3]]
-
-        #prim = [ρ, sqrt(ks.gas.γ), 0.0, ρ/2]
-
+    if ps.x[i, j] <= ps.x1 * 0.25
+        prim = [t2[1], t1[2] - t2[2], 0.0, t2[3]]
+    else
+        prim = [t1[1], 0.0, 0.0, t1[3]]
         s = prim[1]^(1 - ks.gas.γ) / (2 * prim[end])
 
-        κ = 0.2 # vortex strength
+        κ = 0.2
         μ = 0.204
         rc = 0.05
 
-        r = sqrt((ps.xpg[i, j, k, l, 1] - 0.25)^2 + (ps.xpg[i, j, k, l, 2] - 0.5)^2)
+        x0 = 0.8
+        y0 = 0.5
 
-
+        r = sqrt((ps.xpg[i, j, k, l, 1] - x0)^2 + (ps.xpg[i, j, k, l, 2] - y0)^2)
         η = r / rc
 
-        δu = κ * η * exp(μ * (1 - η^2)) * (ps.xpg[i, j, k, l, 2] - 0.5) / r
-        δv = -κ * η * exp(μ * (1 - η^2)) * (ps.xpg[i, j, k, l, 1] - 0.25) / r
+        δu = κ * η * exp(μ * (1 - η^2)) * (ps.xpg[i, j, k, l, 2] - y0) / r
+        δv = -κ * η * exp(μ * (1 - η^2)) * (ps.xpg[i, j, k, l, 1] - x0) / r
         δT = -(ks.gas.γ - 1) * κ^2 / (8 * μ * ks.gas.γ) * exp(2 * μ * (1 - η^2))
 
         T0 = 1 / prim[end]
-
         ρ = prim[1]^(ks.gas.γ - 1) * (T0 + δT) / T0^(1 / (ks.gas.γ - 1))
-        #@show prim[1]
-
-        #@show (1 / (s * 2 * (prim[end] + δλ)))
-
-        #ρ = (1 / (s * 2 * (prim[end] + δλ)))^(1/(ks.gas.γ-1))
         prim1 = [ρ, prim[2] + δu, prim[3] + δv, 1 / (1 / prim[4] + δT)]
 
-        prim .= prim1
-    else
-        t2 = ib_rh(ks.gas.Ma, ks.gas.γ, rand(3))[6]
-        prim = [t2[1], t2[2], 0.0, t2[3]]
-
-        #=MaR = sqrt((ks.gas.Ma^2 * (ks.gas.γ - 1.0) + 2.0) / 
-        (2.0 * ks.gas.γ * ks.gas.Ma^2 - (ks.gas.γ - 1.0)))
-        ratioT =
-            (1.0 + (ks.gas.γ - 1.0) / 2.0 * ks.gas.Ma^2) * (2.0 * ks.gas.γ / 
-            (ks.gas.γ - 1.0) * ks.gas.Ma^2 - 1.0) /
-            (ks.gas.Ma^2 * (2.0 * ks.gas.γ / (ks.gas.γ - 1.0) + (ks.gas.γ - 1.0) / 2.0))
-
-        prim = [
-            ρ * (ks.gas.γ + 1.0) * ks.gas.Ma^2 / ((ks.gas.γ - 1.0) * ks.gas.Ma^2 + 2.0),
-            MaR * sqrt(ks.gas.γ / 2.0) * sqrt(ratioT),
-            0.0,
-            ρ/2 / ratioT,
-        ]=#
+        if r <= rc * 8
+            prim .= prim1
+        end
     end
 
     u0[i, j, k, l, :] .= prim_conserve(prim, ks.gas.γ)
 end
+
+# alternative formulation from dsmc shock-vortex interaction paper
+#=for i in axes(u0, 1), j in axes(u0, 2), k in axes(u0, 3), l in axes(u0, 4)
+    t1, t2 = ib_rh(ks.gas.Ma, ks.gas.γ)
+
+    if ps.x[i, j] <= ps.x1 * 0.25
+        prim = [t2[1], t1[2] - t2[2], 0.0, t2[3]]
+    else
+        prim = [t1[1], 0.0, 0.0, t1[3]]
+        s = prim[1]^(1 - ks.gas.γ) / (2 * prim[end])
+
+        x0 = 1.0
+        y0 = 0.5
+        r = sqrt((ps.xpg[i, j, k, l, 1] - x0)^2 + (ps.xpg[i, j, k, l, 2] - y0)^2)
+
+        vm = 0.1
+        r1 = 0.07
+        r2 = 2 * r1
+
+        rset = r1 / (r1^2 - r2^2)
+        gammaset = (ks.gas.γ - 1) / ks.gas.γ
+        cst = 0.1#5
+        rho1 =
+            (
+                (ks.gas.γ - 1) / cst / ks.gas.γ *
+                vm^2 *
+                rset^2 *
+                (
+                    (0.5 * r1^2 - 0.5 * r2^4 / r1^2 - 2 * r2^2 * log(r1)) -
+                    (0.5 * r2^2 - 0.5 * r2^4 / r2^2 - 2 * r2^2 * log(r2))
+                ) + 1^(ks.gas.γ - 1)
+            )^(1 / (ks.gas.γ - 1))
+        T1 =
+            1.0 +
+            2 *
+            gammaset *
+            rset^2 *
+            vm^2 *
+            (
+                (0.5 * r1^2 - 0.5 * r2^4 / r1^2 - 2 * r2^2 * log(r1)) -
+                (0.5 * r2^2 - 0.5 * r2^4 / r2^2 - 2 * r2^2 * log(r2))
+            )
+
+        if r <= r1
+            vθ = vm * r / r1
+        elseif r1 < r <= r2
+            vθ = vm * (r1) / (r1^2 - r2^2) * (r - r2^2 / r)
+        else
+            vθ = 0
+        end
+
+        @assert vθ >= 0
+
+        cosθ = (ps.xpg[i, j, k, l, 1] - x0) / r
+        sinθ = (ps.xpg[i, j, k, l, 2] - y0) / r
+
+        δu = vθ * sinθ
+        δv = -vθ * cosθ
+        prim[2:3] .= [δu, δv]
+
+        if r <= r1
+            prim[1] =
+                (
+                    ((ks.gas.γ - 1) / cst / ks.gas.γ * vm^2 / 2 / r1^2 * (r^2 - r1^2)) +
+                    rho1^(ks.gas.γ - 1)
+                )^(1 / (ks.gas.γ - 1))
+
+            prim[4] = T1 + 2 * gammaset * vm^2 / r1^2 / 2 * (r^2 - r1^2)
+            prim[4] = 1 / prim[4]
+        elseif r1 < r <= r2
+            prim[1] =
+                (
+                    (ks.gas.γ - 1) / cst / ks.gas.γ *
+                    vm^2 *
+                    rset^2 *
+                    (
+                        (0.5 * r^2 - 0.5 * r2^4 / r^2 - 2 * r2^2 * log(r)) -
+                        (0.5 * r2^2 - 0.5 * r2^4 / r2^2 - 2 * r2^2 * log(r2))
+                    ) + 1^(ks.gas.γ - 1)
+                )^(1 / (ks.gas.γ - 1))
+
+            prim[4] =
+                1.0 +
+                2 *
+                gammaset *
+                rset^2 *
+                vm^2 *
+                (
+                    (0.5 * r^2 - 0.5 * r2^4 / r^2 - 2 * r2^2 * log(r)) -
+                    (0.5 * r2^2 - 0.5 * r2^4 / r2^2 - 2 * r2^2 * log(r2))
+                )
+            prim[4] = 1 / prim[4]
+        end
+    end
+
+    u0[i, j, k, l, :] .= prim_conserve(prim, ks.gas.γ)
+end=#
 
 prob = ODEProblem(dudt!, u0, tspan, p)
 itg = init(prob, Midpoint(), save_everystep = false, adaptive = false, dt = dt)
 
 @showprogress for iter = 1:100#nt
     # limiter
-    #=@inbounds @threads for j = 1:ps.ny
+    @inbounds @threads for j = 1:ps.ny
         for i = 1:ps.nx
             ũ = @view itg.u[i, j, :, :, :]
             positive_limiter(ũ, ks.gas.γ, ps.wp ./ 4, ps.ll, ps.lr)
         end
-    end=#
+    end
 
     step!(itg)
 
-    #=for i in axes(itg.u, 1), j in axes(itg.u, 2)
-        û = iV * itg.u[i, j, 1:3, 1:3, 1][:]
-        su = û[end]^2 / sum(û.^2)
-        isShock = shock_detector(
-            log10(su),
-            ps.deg,
-            -3.0 * log10(ps.deg),
-            8)
+    # filter
+    for i in axes(itg.u, 1), j in axes(itg.u, 2)
+        û = ps.iV * itg.u[i, j, 1:3, 1:3, 1][:]
+        su = û[end]^2 / sum(û .^ 2)
+        isShock = shock_detector(log10(su), ps.deg, -3.0 * log10(ps.deg), 9)
 
         if isShock
-            #@show i j
             for s = 1:4
-                û = iV * itg.u[i, j, 1:3, 1:3, s][:]
-                FR.modal_filter!(û, 2e-4; filter = :l2)
-                uNode = reshape(V * û, 3, 3)
+                û = ps.iV * itg.u[i, j, 1:3, 1:3, s][:]
+                FR.modal_filter!(û, 5e-4; filter = :l2)
+                uNode = reshape(ps.V * û, 3, 3)
                 itg.u[i, j, :, :, s] .= uNode
             end
         end
-    end=#
+    end
 
     # boundary
-    #itg.u[:, 0, :, :, :] .= itg.u[:, ps.ny, :, :, :]
-    #itg.u[:, ps.ny+1, :, :, :] .= itg.u[:, 1, :, :, :]
-    #itg.u[:, 0, :, :, :] .= itg.u[:, 1, :, :, :]
-    #itg.u[:, ps.ny+1, :, :, :] .= itg.u[:, ps.ny, :, :, :]
-
-    itg.u[:, 0, :, 1:end, :] .= itg.u[:, 1, :, end:-1:1, :]
-    itg.u[:, ps.ny+1, :, 1:end, :] .= itg.u[:, ps.ny, :, end:-1:1, :]
-    itg.u[:, 0, :, 1:end, 3] .*= -1
-    itg.u[:, ps.ny+1, :, end:-1:1, 3] .*= -1
-
+    itg.u[:, 0, :, :, :] .= itg.u[:, 1, :, :, :]
+    itg.u[:, ps.ny+1, :, :, :] .= itg.u[:, ps.ny, :, :, :]
     itg.u[ps.nx+1, :, :, :, :] .= itg.u[ps.nx, :, :, :, :]
-    #itg.u[0, :, :, :, :] .= itg.u[1, :, :, :, :]
+    #itg.u[:, 0, :, 1:end, :] .= itg.u[:, 1, :, end:-1:1, :]
+    #itg.u[:, ps.ny+1, :, 1:end, :] .= itg.u[:, ps.ny, :, end:-1:1, :]
+    #itg.u[:, 0, :, 1:end, 3] .*= -1
+    #itg.u[:, ps.ny+1, :, end:-1:1, 3] .*= -1
 end
 
 begin
@@ -315,10 +371,7 @@ begin
             sol[idx, idy, 4] = 1 / sol[idx, idy, 4]
         end
     end
-
-    #contourf(x, y, sol[:, :, 4], aspect_ratio=1, legend=true)
-    #plot(x[:, 1], sol[:, end÷2+1, 1])
 end
 
-contourf(x, y, sol[:, :, 1], aspect_ratio = 1, legend = true)
-plot(x[:, 1], sol[:, end÷2+1, 4])
+contourf(x[:, 1], y[1, :], sol[:, :, 1]', aspect_ratio = 1, legend = true)
+plot(x[:, 1], sol[:, end÷2+1, 1])
